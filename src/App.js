0d6429308bd33baa5d4745aa672a9e4a249dc01b
Jake Gliddon <jakegliddon.it@gmail.com>
2022-04-30 10:52:04 +0100

    going to push

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://smart-brain-live-jake-api.herokuapp.com:3000/image', {
+          fetch('https://smart-brain-live-jake-api.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
